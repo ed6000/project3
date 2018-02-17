3f1cd717a54359834ed098fb665fcdecadde1d16
jim113453 <31411325+jim@users.noreply.github.com>
2018-02-17 16:11:11 -0500

    created basic skeleton for homepage component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.queryUsers = this.queryUsers.bind(this)
+  }
+
+  queryUsers() {
+    axios.get('') //make a call to users in the DB
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+
   render() {
     return (
       <div>
-        This is the Home route
+        <header>Welcome to PlanIt!</header>
+
+        {/* sign in form here
+        register link here */}
+
+        <footer>Copyright &#169; 2018 mess  All Rights Reserved</footer>
       </div>
-      )
+    )
   }
 }
